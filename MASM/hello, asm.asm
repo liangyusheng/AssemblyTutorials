0f62e55066697edc0c40ec963aeed6870040df64
liangyusheng <liangyusheng@myself.com>
2021-08-11 11:50:51 +0800

    Demo | 将文件中 tab 替换为 space

--- MASM/hello, asm.asm
+++ MASM/hello, asm.asm
@@ -1,32 +1,32 @@
 assume cs:code, ds:data
 
 data segment
-	db	'hello, asm!', 0
+    db  'hello, asm!', 0
 data ends
 
 code segment
-	start:	mov ax, data
-			mov ds, ax
-			
-			mov ax, 0b800h
-			mov es, ax
-			
-			mov ch, 0
-			mov bx, 0
-			; 15 行， 35 列
-			mov si, 15 + 35
-			
-	print:	mov cl, ds:[bx]
-			mov es:[si], cl
-			inc si
-			; set word color.
-			mov es:[si], byte ptr 00000101b
-			inc bx
-			inc si
-			jcxz exit
-			jmp print
+    start:  mov ax, data
+            mov ds, ax
+            
+            mov ax, 0b800h
+            mov es, ax
+            
+            mov ch, 0
+            mov bx, 0
+            ; 15 行， 35 列
+            mov si, 15 + 35
+            
+    print:  mov cl, ds:[bx]
+            mov es:[si], cl
+            inc si
+            ; set word color.
+            mov es:[si], byte ptr 00000101b
+            inc bx
+            inc si
+            jcxz exit
+            jmp print
 
-	exit:	mov ax, 4c00h
-			int 21h
+    exit:   mov ax, 4c00h
+            int 21h
 code ends
 end start
